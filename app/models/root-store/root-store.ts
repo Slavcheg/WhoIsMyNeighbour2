9c039ba68f5ee715e8e802a743fb7dfc1c958f66
Georgi Slavchev <georgi.v.slavchev@gmail.com>
2020-07-20 15:57:45 +0300

    find distance

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -24,7 +24,36 @@ export const RootStoreModel = types.model("RootStore").props({
 .views(self => ({
     getCountry(){
         return values(self.countries[0].topLevelDomain)
-    }
+    },
+
+    getDistanceByLatLong(lat1, lon1, lat2, lon2){
+        const R = 6371e3; // metres
+        const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+        const φ2 = lat2 * Math.PI/180;
+        const Δφ = (lat2-lat1) * Math.PI/180;
+        const Δλ = (lon2-lon1) * Math.PI/180;
+
+        const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        const d = Math.round((R * c)/1000); // in km
+
+        console.log(d)
+        return d
+    },
+
+    getDistanceByAlpha3Code(country1, country2){
+        const lat1 = self.countries.find(country => country.alpha3Code === country1).latlng[0]
+        const lon1 = self.countries.find(country => country.alpha3Code === country1).latlng[1]
+        const lat2 = self.countries.find(country => country.alpha3Code === country2).latlng[0]
+        const lon2 = self.countries.find(country => country.alpha3Code === country2).latlng[1]
+
+        return this.getDistanceByLatLong(lat1, lon1, lat2, lon2)
+    },
+
+    
 }))
 /**
  * The RootStore instance.
